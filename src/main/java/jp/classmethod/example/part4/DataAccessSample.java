3b9346e8d45e12f56b668b07ca20f3514e747ea4
Daisuke Miyamoto <miyamoto.daisuke@classmethod.jp>
2016-04-07 15:53:56 +0900

    Add data access application frame

---
+++ src/main/java/jp/classmethod/example/part4/DataAccessSample.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2013-2016 Classmethod, Inc..
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package jp.classmethod.example.part4;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@ComponentScan
+public class DataAccessSample {
+	
+	public static void main(String[] args) {
+		try (ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(DataAccessSample.class)) {
+			DataAccessSample das = context.getBean(DataAccessSample.class);
+			das.execute();
+		}
+	}
+	
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	
+	@Transactional
+	public void execute() {
+		// do some work
+	}
+}
---
+++ src/main/java/jp/classmethod/example/part4/config/DataSourceConfiguration.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2013-2016 Classmethod, Inc..
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package jp.classmethod.example.part4.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class DataSourceConfiguration {
+	
+	@Bean
+	public DriverManagerDataSource dataSource() {
+		DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+		driverManagerDataSource.setDriverClassName(com.mysql.jdbc.Driver.class.getName());
+		driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/berserker");
+		driverManagerDataSource.setUsername("root");
+		driverManagerDataSource.setPassword("");
+		return driverManagerDataSource;
+	}
+	
+	@Bean
+	public DataSourceTransactionManager transactionManager() {
+		return new DataSourceTransactionManager(dataSource());
+	}
+	
+	@Bean
+	public JdbcTemplate jdbcTemplate() {
+		return new JdbcTemplate(dataSource());
+	}
+}
