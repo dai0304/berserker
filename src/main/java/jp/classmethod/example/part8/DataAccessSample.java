4ba493955815d577ef99f6d276d10dc9050cf8fe
Daisuke Miyamoto <miyamoto.daisuke@classmethod.jp>
2016-04-07 17:26:19 +0900

    Revert "Modify data access example to fail"
    This reverts commit beac4e35384f3fe289c637125015fbeba6690df8.

--- src/main/java/jp/classmethod/example/part8/DataAccessSample.java
+++ src/main/java/jp/classmethod/example/part8/DataAccessSample.java
@@ -15,12 +15,16 @@
  */
 package jp.classmethod.example.part8;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -45,9 +49,46 @@ public class DataAccessSample {
 	
 	@Transactional
 	public void execute() {
-		// 成功するDB書き込み操作
-		userRepos.save(new User("torazuka", "$2a$10$fx33wHST4ecwp53MB5QvROQtIYwkdCU2O3XJK6LuCmm415dRncluC"));
-		// からの失敗
-		throw new RuntimeException();
+		boolean flag = userRepos.exists("yokota");
+		
+		// create
+		logger.info("Create user");
+		if (flag) {
+			userRepos.save(new User("watanabe", "$2a$10$MHPqWJ61alnBlUbvjEGK/uWRvwtYzolWCuFXW8YMJkT54HUB0H9iq"));
+		} else {
+			userRepos.save(new User("yokota", "$2a$10$nkvNPCb3Y1z/GSearD7s7OBdS9BoLBss3D4enbFQIgNJDvr0Xincm"));
+		}
+		
+		// read
+		logger.info("List user");
+		Iterable<User> all = userRepos.findAll();
+		for (User user : all) {
+			logger.info("  {}", user);
+		}
+		
+		logger.info("List user filtered by length");
+		Page<User> p = userRepos.findByUsernameMaxLength(6, new PageRequest(0, 2));
+		List<User> users = p.getContent();
+		assert users.size() <= 2;
+		for (User user : users) {
+			logger.info("  {}", user);
+		}
+		
+		logger.info("Get user");
+		User miyamoto = userRepos.findOne("miyamoto");
+		logger.info("  {}", miyamoto);
+		
+		// update
+		logger.info("Update user");
+		miyamoto.setPassword("$2a$10$vxq4n5VB4bsgUlBK9DXV9edhX911Qz/5iYqjLi/6qZPp8Xl7ZACKC");
+		userRepos.save(miyamoto);
+		
+		// delete
+		logger.info("Delete user");
+		if (flag) {
+			userRepos.delete("yokota");
+		} else {
+			userRepos.delete("watanabe");
+		}
 	}
 }
