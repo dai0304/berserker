3e6a464349a276884e15a2a34e45fc833b483b0d
Daisuke Miyamoto <miyamoto.daisuke@classmethod.jp>
2016-04-08 18:19:31 +0900

    Remove CRUD example

--- src/main/java/jp/classmethod/example/berserker/BerserkerApplication.java
+++ src/main/java/jp/classmethod/example/berserker/BerserkerApplication.java
@@ -15,7 +15,6 @@
  */
 package jp.classmethod.example.berserker;
 
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -25,11 +24,8 @@ import jp.classmethod.example.berserker.model.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,7 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class BerserkerApplication implements CommandLineRunner {
+public class BerserkerApplication {
 	
 	private static Logger logger = LoggerFactory.getLogger(BerserkerApplication.class);
 	
@@ -62,51 +58,5 @@ public class BerserkerApplication implements CommandLineRunner {
 			.map(Object::toString)
 			.collect(Collectors.joining(","));
 		return ResponseEntity.ok(result);
-	}
-	
-	@Override
-	@Transactional
-	public void run(String... args) throws Exception {
-		boolean flag = userRepos.exists("yokota");
-		
-		// create
-		logger.info("Create user");
-		if (flag) {
-			userRepos.save(new User("watanabe", "$2a$10$MHPqWJ61alnBlUbvjEGK/uWRvwtYzolWCuFXW8YMJkT54HUB0H9iq"));
-		} else {
-			userRepos.save(new User("yokota", "$2a$10$nkvNPCb3Y1z/GSearD7s7OBdS9BoLBss3D4enbFQIgNJDvr0Xincm"));
-		}
-		
-		// read
-		logger.info("List user");
-		Iterable<User> all = userRepos.findAll();
-		for (User user : all) {
-			logger.info("  {}", user);
-		}
-		
-		logger.info("List user filtered by length");
-		Page<User> p = userRepos.findByUsernameMaxLength(6, new PageRequest(0, 2));
-		List<User> users = p.getContent();
-		assert users.size() <= 2;
-		for (User user : users) {
-			logger.info("  {}", user);
-		}
-		
-		logger.info("Get user");
-		User miyamoto = userRepos.findOne("miyamoto");
-		logger.info("  {}", miyamoto);
-		
-		// update
-		logger.info("Update user");
-		miyamoto.setPassword("$2a$10$vxq4n5VB4bsgUlBK9DXV9edhX911Qz/5iYqjLi/6qZPp8Xl7ZACKC");
-		userRepos.save(miyamoto);
-		
-		// delete
-		logger.info("Delete user");
-		if (flag) {
-			userRepos.delete("yokota");
-		} else {
-			userRepos.delete("watanabe");
-		}
 	}
 }
