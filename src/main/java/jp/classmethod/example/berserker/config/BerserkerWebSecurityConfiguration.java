f40d78e25f2333b3e2d0bc4b100cd095ca37ac9e
Daisuke Miyamoto <miyamoto.daisuke@classmethod.jp>
2016-06-01 17:42:14 +0900

    Use form login

--- src/main/java/jp/classmethod/example/berserker/config/BerserkerWebSecurityConfiguration.java
+++ src/main/java/jp/classmethod/example/berserker/config/BerserkerWebSecurityConfiguration.java
@@ -15,6 +15,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -33,6 +34,14 @@ public class BerserkerWebSecurityConfiguration extends WebSecurityConfigurerAdap
 	@Autowired
 	DataSource dataSource;
 	
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.anyRequest().authenticated();
+		
+		http.formLogin();
+	}
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
